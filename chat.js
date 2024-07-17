3c1ca5a7e9e1311e6bbd2b532efcc8e8122f3968
もどっぐ <yuuta4083@gmail.com>
2024-07-18 07:21:13 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,37 +1,40 @@
-    $(function(){
-        $("#response").html("Response Values");
+function sendWebhook(event) {
+  event.preventDefault();
+  
+  const webhookUrl = "https://discord.com/api/webhooks/1263255244088021012/PhlqNXm2GnT1lJYIqTHfOWQb0exmQRwHq2cozQ6Qs4az8qvx_bZW0DlLqwDDuQr5TdZr";
+  const message = document.getElementById("message").value;
 
-        $("#button").click( function(){
-            var url = "https://discord.com/api/webhooks/1263255244088021012/PhlqNXm2GnT1lJYIqTHfOWQb0exmQRwHq2cozQ6Qs4az8qvx_bZW0DlLqwDDuQr5TdZr";
+  if (!webhookUrl || !interval || !times) {
+    alert("Missing parameters.");
+    return;
+  }
 
-                var JSONdata = {
-                    value1: $("#value1").val(),
-                    value2: $("#value2").val()      
-                };
-
-            alert(JSON.stringify(JSONdata));
-
-            $.ajax({
-                type : 'post',
-                url : url,
-                data : JSON.stringify(JSONdata),
-                contentType: 'application/JSON',
-                dataType : 'JSON',
-                scriptCharset: 'utf-8',
-                success : function(data) {
-
-                    // Success
-                    alert("success");
-                    alert(JSON.stringify(data));
-                    $("#response").html(JSON.stringify(data));
-                },
-                error : function(data) {
-
-                    // Error
-                    alert("error");
-                    alert(JSON.stringify(data));
-                    $("#response").html(JSON.stringify(data));
-                }
-            });
-        })
-    })
+  if (!message && fileInput.files.length == 0) {
+    alert("Missing parameters.");
+    return;
+  }
+  
+  for (let i = 0; i < times; i++) {
+    const modifiedMessage = applyOption(message, option);
+    const payload = {
+      content: modifiedMessage
+    };
+    const formData = new FormData();
+    formData.append("payload_json", JSON.stringify(payload));
+    if (fileInput.files.length > 0) {
+      formData.append("file", fileInput.files[0]);
+    }
+    
+    setTimeout(() => {
+      fetch(webhookUrl, {
+        method: "POST",
+        body: formData
+      }).then(response => {
+        console.log("Message sent successfully!");
+      }).catch(error => {
+        console.error("Error sending message");
+      });
+    }, interval);
+  }
+  alert(`send-ok!`);
+}
